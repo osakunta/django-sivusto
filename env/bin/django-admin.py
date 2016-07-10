594341577276ba242acd2ad7c95c9afb054331d3
V-Kopio <ville.kopio@gmail.com>
2016-07-10 17:31:05 +0300

    Deleted virtualenv

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kopio/WebProjects/django/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- env/lib/python3.5/_weakrefset.py
+++
@@ -1,1 +0,0 @@
-/home/kopio/.anaconda3/lib/python3.5/_weakrefset.py
