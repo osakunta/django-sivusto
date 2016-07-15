02b58fd1739f5d5d1c87d9aacc4b77f35411eb19
V-Kopio <ville.kopio@gmail.com>
2016-07-15 05:08:26 +0300

    Made changes to app.js - footer sticks to bottom and height equalizers are now dynamic.

--- assets/input/js/app.js
+++ assets/input/js/app.js
@@ -1,16 +1,42 @@
 // Takes a class name as a parameter and makes every container with said class
 // as tall as the one of them which has the longest height.
 function equalHeight(className) {
-    $( document ).ready(function() {
-        var heights = $(className).map(function() {
-            return $(this).height();
-        }).get(),
+    var heights;
+    
+    $(className).removeAttr("style");
 
-        maxHeight = Math.max.apply(null, heights);
+    heights = $(className).map(function() {
+        return $(this).height();
+    }).get(),
+        
+    maxHeight = Math.max.apply(null, heights);
 
-        $(className).height(maxHeight);
-    });
+    $(className).height(maxHeight);
 }
 
-equalHeight(".equalize-footer");
-equalHeight(".equalize-featured");
+
+// function to set the height on fly
+function autoHeight() {
+    $('#content').css('min-height', 0);
+    $('#content').css('min-height', (
+        $(document).height() 
+        - $('#header').outerHeight() 
+        - $('#footer').outerHeight()
+    ));
+    
+}
+
+// onDocumentReady function bind
+$(document).ready(function() {
+    autoHeight();
+    equalHeight(".equalize-footer");
+    equalHeight(".equalize-featured");
+});
+
+// onResize bind of the function
+$(window).resize(function() {
+    autoHeight();
+    equalHeight(".equalize-footer");
+    equalHeight(".equalize-featured");
+});
+
