31087b0d9cec8a5137c5df267167d042d9fd6a81
V-Kopio <ville.kopio@gmail.com>
2017-04-23 22:59:16 +0300

    Cache gallery views for a day

--- gallery/views.py
+++ gallery/views.py
@@ -3,15 +3,20 @@ import os
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.static import serve
+from django.views.decorators.cache import cache_page
 
 path="media/gallery-images"
 
+for_a_day = 60**2 * 24
+
+@cache_page(for_a_day)
 @login_required
 def years(request):
     year_list = os.listdir(path)
     year_list.sort(reverse = True)
     return render(request, 'years.html', {'years': year_list})
 
+@cache_page(for_a_day)
 @login_required
 def gallery_list(request, year=2017):
     galleries_path = path + '/' + year
@@ -19,6 +24,7 @@ def gallery_list(request, year=2017):
     galleries.sort(reverse = True, key = lambda x: os.path.getmtime(galleries_path + '/' + x))
     return render(request, 'galleries.html', {'galleries': galleries, 'year': year})
 
+@cache_page(for_a_day)
 @login_required
 def gallery(request, year, gallery):
     images = os.listdir(path + '/' + year + '/' + gallery)
