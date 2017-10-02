3ad497c033255f32fc1a6adb01b323894f6f6ca3
V-Kopio <ville.kopio@gmail.com>
2017-10-02 22:20:16 +0300

    Added static collecting to production

--- bin/production.sh
+++ bin/production.sh
@@ -6,6 +6,9 @@ python manage.py makemigrations
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Collect static files to one location"
+python manage.py collectstatic
+
 echo "Starting Gunicorn"
 exec gunicorn sato.wsgi:application \
     --bind 0.0.0.0:8010 \
