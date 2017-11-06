812d8feac913787ea04365717ee79a482b31048b
V-Kopio <ville.kopio@gmail.com>
2017-11-06 22:02:28 +0200

    Specified logfiles for gunicorn

--- bin/production.sh
+++ bin/production.sh
@@ -12,6 +12,8 @@ python manage.py collectstatic --noinput
 echo "Starting Gunicorn"
 exec gunicorn sato.wsgi:application \
     --bind 0.0.0.0:8010 \
+    --error-logfile /var/log/gunicorn/error.log \
+    --access-logfile /var/log/gunicorn/access.log \
     --log-level=DEBUG \
     --workers 3 \
     -t 120
