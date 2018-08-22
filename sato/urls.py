301f41e7c267c135b969e532134bd11f9511cb3b
V-Kopio <ville.kopio@gmail.com>
2018-08-22 14:25:23 +0300

    Updated site urls by removing unused urls and imports

--- sato/urls.py
+++ sato/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -11,13 +11,15 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.views.static import serve
 from . import views
-import sato.settings
+
 
 @login_required
 def protected_serve(request, path, document_root=settings.MEDIA_ROOT + '/gallery-images/', show_indexes=False):
     return serve(request, path, document_root, show_indexes)
 
+
 admin.autodiscover()
+
 
 protected_media = [
     url(r'^media/gallery-images/(?P<path>.*)$', protected_serve),
@@ -26,9 +28,6 @@ protected_media = [
 
 urlpatterns = protected_media + i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),  # NOQA
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
-        {'sitemaps': {'cmspages': CMSSitemap}}),
-    url(r'^select2/', include('django_select2.urls')),
     url(r'^login/$', auth_views.login, {'template_name': 'auth/login.html'} , name='login'),
     url(r'^logout/$', views.logout_user),
     url(r'^hallituspalaute/', include('hallituspalaute.urls')),
