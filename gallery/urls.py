79bfbcf23779744fca87c33b6824e05aceb0cc99
vkopio <ville.kopio@gmail.com>
2018-09-22 12:54:19 +0300

    Added url path to a variable

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.na),
+    url(r'^(?P<gallery_path>.*)$', views.na),
     # url(r'^$', views.years),
     # url(r'^(?P<year>[0-9]{4})/$', views.gallery_list),
     # url(r'^(?P<year>[0-9]{4})/(?P<gallery>.*)/$', views.gallery),
