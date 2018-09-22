0e1516c0296f1f1e6793b8f8022ad8aa55bfd5c4
vkopio <ville.kopio@gmail.com>
2018-09-22 13:00:26 +0300

    Remove old gallery code

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,8 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<gallery_path>.*)$', views.na),
-    # url(r'^$', views.years),
-    # url(r'^(?P<year>[0-9]{4})/$', views.gallery_list),
-    # url(r'^(?P<year>[0-9]{4})/(?P<gallery>.*)/$', views.gallery),
+    url(r'^(?P<gallery_path>.*)$', views.gallery_list),
 ]
