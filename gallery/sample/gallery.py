e854f7f0534e4f6dcf1c53f5b3a1e53b17de85da
vkopio <ville.kopio@gmail.com>
2018-09-22 09:47:34 +0300

    Added initial new gallery class

---
+++ gallery/sample/gallery.py
@@ -0,0 +1,27 @@
+import os
+
+
+class Gallery:
+    def __init__(self, gallery_path):
+        self.image_root = "/usr/src/app/gallery-images/"
+        self.thumb_root = "/usr/src/app/media/gallery-thumbs/"
+        self.gallery_path = gallery_path
+        self.subgalleries = []
+        self.images = []
+
+        self.__set_images_and_subgalleries(self.absolute_gallery_path())
+
+    def absolute_gallery_path(self):
+        return self.image_root + self.gallery_path
+
+    def absolute_gallery_thumbs(self):
+        return self.thumb_root + self.gallery_path
+
+    def __set_images_and_subgalleries(self, gallery_path):
+        dir_entries = os.scandir(gallery_path)
+
+        for dir_entry in dir_entries:
+            if dir_entry.is_dir():
+                self.subgalleries.append(dir_entry)
+            elif dir_entry.is_file():
+                self.images.append(dir_entry)
