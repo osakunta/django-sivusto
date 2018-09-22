790f34642b0ad59c8718e9b463038a90f30f0a55
vkopio <ville.kopio@gmail.com>
2018-09-22 19:41:44 +0300

    Raise Http404 if gallery dir not found

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from .sample.gallery import Gallery
 
 
 @login_required
 def gallery_list(request, gallery_path):
-    gallery = Gallery(gallery_path, '/gallery/')
+    try:
+        gallery = Gallery(gallery_path, '/gallery/')
+    except FileNotFoundError:
+        raise Http404
+
     current_path = request.path + '/' if not request.path.endswith('/') else request.path
     context = {'gallery': gallery, 'current_path': current_path}
 
