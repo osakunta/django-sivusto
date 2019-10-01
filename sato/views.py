3f4b4a69a27a45d50d37deeb752428466dd75867
vkopio <ville.kopio@gmail.com>
2019-10-01 12:38:31 +0300

    Removed dead code and fixed handlers

--- sato/views.py
+++ sato/views.py
@@ -1,31 +1,7 @@
 from django.http import *
-from django.shortcuts import render_to_response, render
-from django.template import RequestContext
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from django.contrib.auth import logout
 
-def login_user(request):
-    messages.add_message(request, messages.INFO, 'Hello world.')
-    username = password = ''
-
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-
-                return HttpResponseRedirect('/')
-            else:
-                messages.error(request, 'The account is disabled.')
-
-        else:
-            messages.error(request, 'Wrong username or password.')
-
-    return render_to_response('login.html', context_instance=RequestContext(request))
 
 def logout_user(request):
     logout(request)
@@ -33,9 +9,9 @@ def logout_user(request):
     return HttpResponseRedirect('/')
 
 
-def handler404(request):
+def handler404(request, *args, **kwargs):
     return render(request, '404.html', status=404)
 
 
-def handler500(request):
+def handler500(request, *args, **kwargs):
     return render(request, '500.html', status=500)
