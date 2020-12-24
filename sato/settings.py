e8c861208a1d9b03b4629ca1285473f22827c9f0
Ville Kopio <ville.kopio@kodan.fi>
2020-12-25 01:55:43 +0200

    Add user handlers.

--- sato/settings.py
+++ sato/settings.py
@@ -116,6 +116,18 @@ SOCIAL_AUTH_AUTH0_SCOPE = [
     'email'
 ]
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 AUTHENTICATION_BACKENDS = {
     'auth0login.auth0backend.Auth0',
     'django.contrib.auth.backends.ModelBackend'
