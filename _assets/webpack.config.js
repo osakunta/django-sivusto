7ca9527dd33618a6ecf6207861d560f35dacf328
Ville Kopio <ville.kopio@kodan.fi>
2021-01-06 15:23:58 +0200

    Setup sass, fonts and images with webpack.

--- _assets/webpack.config.js
+++ _assets/webpack.config.js
@@ -18,6 +18,22 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
