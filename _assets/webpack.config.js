c77b475dcf5c5bf6201aef04d476ec12cc5aa86e
Ville Kopio <ville.kopio@kodan.fi>
2021-01-06 15:31:25 +0200

    Use file-loader to not have random file names.

--- _assets/webpack.config.js
+++ _assets/webpack.config.js
@@ -28,11 +28,17 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]',
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]',
+        },
       },
     ],
   },
