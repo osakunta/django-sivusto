59b80e4921a3ea88619fa56b221daf5e735e4058
Ville Kopio <ville.kopio@kodan.fi>
2021-01-06 16:12:51 +0200

    Add missing assets and simplify image dir structure.

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,8 @@ import 'bootstrap-sass';
 import "@fortawesome/fontawesome-free/js/all";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./sass/styles.sass";
-
+import "./img/favicon.ico";
+import "./img/logos/main.png";
 import app from './app';
 import calendar from './calendar';
 
