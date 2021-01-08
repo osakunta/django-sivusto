c60d039b9844f7cba7a6fc5981a30910e9bb416d
Ville Kopio <ville.kopio@kodan.fi>
2021-01-08 22:22:21 +0200

    Remove the instafeed for now.

--- assets/app.js
+++ assets/app.js
@@ -11,15 +11,6 @@ function autoHeight() {
   ));
 }
 
-// Settings for Instafeed.js
-// var satoInstafeed = new Instafeed({
-//   get: 'user',
-//   userId: '1500056937',
-//   clientId: '26baf6a598e64f5e86bf6369e849f3eb',
-//   accessToken: '1500056937.1677ed0.9dc272018f224b0fa4cf75dc26209c49',
-//   limit: 6
-// });
-
 export default function app() {
   autoHeight();
 
