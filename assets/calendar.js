ef3648bf201451992e0315960cd003d23a1fc088
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-11-30 18:55:48 +0200

    Update calendar id's to new ones
    Hopefully this works

--- assets/calendar.js
+++ assets/calendar.js
@@ -21,23 +21,19 @@ function weekCalendarView() {
 
 const googleCalendars = [
   {
-    googleCalendarId: 'tqbg6bgc6r00hbs4p3bt6h1p4g@group.calendar.google.com',
+    googleCalendarId: 'c_9d338487979b15952164e8f65f4904a79bfdc91ee3d99b8bd0ffd7f72fe0098f@group.calendar.google.com',
     className: 'gcal-sports',
   },
   {
-    googleCalendarId: 'c3f5fekmh2qfloegr4okfghk5k@group.calendar.google.com',
-    className: 'gcal-service',
-  },
-  {
-    googleCalendarId: 'boecmjgkjsuibnnj2op4c94ags@group.calendar.google.com',
+    googleCalendarId: 'c_0e61cd901383f3d7ae9ff52f57dceae240a1dac3c9b5e426448b8751bf7f4bce@group.calendar.google.com',
     className: 'gcal-events',
   },
   {
-    googleCalendarId: 'r151te2dmi4sfp130iahaj2vro@group.calendar.google.com',
+    googleCalendarId: 'c_a2c54e1bc944a0ffeed775cf8568616bad5ab1141f1a7c4c3ca4cececd59754d@group.calendar.google.com',
     className: 'gcal-reserves',
   },
   {
-    googleCalendarId: 'aojklsb36n1vpmesl2tqv1gp6o@group.calendar.google.com',
+    googleCalendarId: 'c_e0a144f6f0324c18dd1ac93ea19f20cac923aa7244672adad99a165f2e885c7d@group.calendar.google.com',
     className: 'gcal-meetings',
   },
 ];
